a38bbb9b9fd40b64dc9404477461659bd0a2957d
Tejasvi Urkande <113376160+tejasviurka@users.noreply.github.com>
2023-06-25 11:57:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('song/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItem = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Let me Love you", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "Hawayein", filePath: "song/2.mp3", coverPath: "cover/2.jpg"},
+    {songName: "Pehla Pyarr", filePath: "song/3.mp3", coverPath: "cover/3.jpg"},
+    {songName: "O Bedardeya", filePath: "song/4.mp3", coverPath: "cover/4.jpg"},
+    {songName: "Raatan Lambiyaan", filePath: "song/5.mp3", coverPath: "cover/5.jpg"},
+    {songName: "Ram Siya Ram", filePath: "song/6.mp3", coverPath: "cover/6.jpg"},
+    {songName: "Deva Deva", filePath: "song/7.mp3", coverPath: "cover/7.jpg"},
+    {songName: "Coka 2.0", filePath: "song/8.mp3", coverPath: "cover/8.jpg"},
+    {songName: "Blue Eyes", filePath: "song/9.mp3", coverPath: "cover/9.jpg"},
+    {songName: "Brown Rang", filePath: "song/10.mp3", coverPath: "cover/10.jpg"},
+]
+
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `song/${songIndex+1}.mp3`;
+        masterSongName.innerText = song[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `song/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `song/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
